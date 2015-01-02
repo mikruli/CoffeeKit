d99f966109a86756b30e16698bc369d1ba5cf677
Dejan Kumric <dejankumric@Dejans-MacBook-Pro.local>
2015-01-02 09:17:31 +0100

    Initial Commit

---
+++ CoffeeKit_gh/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CoffeeKit_gh
+//
+//  Created by Dejan Kumric on 1/2/15.
+//  Copyright (c) 2015 Dejan Kumric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
